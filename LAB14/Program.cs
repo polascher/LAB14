2e0ecc594dd65986047d64399a3f3cb8ae178fa8
polascher <polascher@gmail.com>
2021-05-31 18:36:50 +0700

    LAB14

---
+++ LAB14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LAB14
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const int M = 10;
+            int[,] mas = new int[M, M];
+
+            Console.WriteLine("Введите минимальное значение: ");
+            int min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите максимальное значение: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            Random r = new Random();
+
+            for (int i = 0; i < M; i++)
+                for (int j = 0; j < M; j++)
+                    mas[i, j] = r.Next(min, max);
+
+            for(int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write(mas[i, j]);
+                    Console.Write(' ');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
